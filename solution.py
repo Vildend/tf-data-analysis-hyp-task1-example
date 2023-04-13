8e55ec633d6186bdb042f0a85061368f7cd5158d
Aynur Gatiyatullin <vildend@mail.ru>
2023-04-13 19:25:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,6 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    value = 0.06
+    stat, pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt])
+    return pval < value
