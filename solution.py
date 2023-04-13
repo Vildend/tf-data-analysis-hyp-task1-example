8cc060df8da4c2b59cc719b9e8f9e4fc971edb4b
Aynur Gatiyatullin <vildend@mail.ru>
2023-04-13 19:28:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 
+from statsmodels.stats.proportion import proportions_ztest
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 897901830 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
     value = 0.06
-    stat, pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt])
+    pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt]).pvalue
     return pval < value
